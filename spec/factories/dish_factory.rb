19e4c2ab1d594eb58a14a255a6c5e0b283802d43
wayne <wayne@futureworkz.com>
2015-05-25 15:43:06 +0700

    Set up factorygirl for dishes

---
+++ spec/factories/dish_factory.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :dish do
+    title       'chicken rice'
+    description '1 whole chicken, pandan leave'
+    cost        30
+    pax         2
+    vegetarian  false
+    published   true
+  end
+end
