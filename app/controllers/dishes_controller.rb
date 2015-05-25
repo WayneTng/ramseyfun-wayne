9cd35d9ddfb0a3382fa51bf2e58b860d4b6f8dac
wayne <wayne@futureworkz.com>
2015-05-25 16:40:24 +0700

    set up show test case and controller

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -3,4 +3,12 @@ class DishesController < ApplicationController
   def index
     @dishes = Dish.all
   end
+
+  def show
+    @dishes = Dish.find(dish_id)
+  end
+
+  def dish_id
+    params.require(:id)
+  end
 end
--- spec/controllers/dish_controller_spec.rb
+++ spec/controllers/dish_controller_spec.rb
@@ -10,4 +10,13 @@ RSpec.describe DishesController, type: :controller do
       expect(assigns(:dishes).size).to eq dishes.size
     end
   end 
+
+  describe '#show' do
+    let!(:dishes){create(:dish)}
+
+    it 'it get a product id to display' do
+      get :index, id: dishes
+      expect(assigns(:dishes).id).to eq dishes.id
+    end
+  end
 end
