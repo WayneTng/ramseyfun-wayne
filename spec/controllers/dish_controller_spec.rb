471fd879d1099c92864697d850d4611622203eac
wayne <wayne@futureworkz.com>
2015-05-25 15:44:34 +0700

    set up routes, test spec for dishes_controller#index

---
+++ spec/controllers/dish_controller_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe DishesController, type: :controller do
+
+  describe '#index' do
+    let!(:dishes){create_list(:dish, 2)}
+    
+    it 'it get a list of dishes from product controller' do
+      get :index
+      expect(assigns(:dishes).size).to eq dishes.size
+    end
+  end 
+end
