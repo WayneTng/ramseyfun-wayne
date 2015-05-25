fa5db3534dc131cc536b2c1ca3b29ca737e4207d
wayne <wayne@futureworkz.com>
2015-05-25 16:08:15 +0700

    Install gem for pg and set up configuration for postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,5 @@
-# encoding: UTF-8 # This file is auto-generated from the current state of the database. Instead
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
@@ -11,6 +12,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20150525073937) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "dishes", force: :cascade do |t|
     t.string   "title"
