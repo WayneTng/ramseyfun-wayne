d21559ca883ac4c9ae2d59e4e4a64afacc82e891
wayne <wayne@futureworkz.com>
2015-05-26 22:45:25 +0700

    Validate model cost to ensure that cost is greater_than_or_equal_to 0

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Dish, type: :model do
   context 'Validation' do
     it{ should validate_presence_of(:title) }
     it{ should validate_presence_of(:description) }
+    it{ should validate_numericality_of(:cost).is_greater_than_or_equal_to(0)}
   end
 
   context 'Association' do
