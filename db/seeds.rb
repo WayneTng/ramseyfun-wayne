e452b290914852904f9f5716933b097f4e6c737a
wayne <wayne@futureworkz.com>
2015-05-26 22:19:38 +0700

    Add destroy_all for fan in db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+Fan.destroy_all
 Dish.destroy_all
 
 Dish.create!(title: 'Chicken Rice',
