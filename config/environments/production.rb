574061e0770120222c7b9ecbda282fb395d971a6
wayne <wayne@futureworkz.com>
2015-05-26 15:06:38 +0700

    Add mandrill config to production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,15 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
   config.action_mailer.default_url_options = { host: 'ramseyfan-wayne.herokuapp.com' }
+
+  config.action_mailer.smtp_settings = { 
+    address: "smtp.mandrillapp.com",
+    port: "587",
+    enable_starttls_auto: true,
+    user_name: ENV["MANDRILL_USERNAME"],
+    password: ENV["MANDRILL_APIKEY"],
+    domain: "ramseyfan-wayne.herokuapp.com"
+  }
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like
