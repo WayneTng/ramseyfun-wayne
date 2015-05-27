e75cd1b24798ab3cf9359f4c7aec64aed58f7dde
wayne <wayne@futureworkz.com>
2015-05-27 18:07:31 +0700

    Add validation and logic for cost and pax for dish and my_dishes view

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,7 +1,8 @@
 class Dish < ActiveRecord::Base
   belongs_to :fan
   validates :title, :description, :fan_id, presence: true
-  validates_numericality_of :cost, greater_than_or_equal_to: 0
+  validates_numericality_of :cost, greater_than_or_equal_to: 0, allow_nil: true
+  validates_numericality_of :pax, greater_than_or_equal_to: 0, only_integer: true, allow_nil: true
   scope :published, -> { where(published: true) } 
   scope :search_title_and_description, ->(search_title){ where( "upper(title) LIKE ? OR upper(description) LIKE ?", "%#{search_title.upcase}%", "%#{search_title.upcase}%" ) }
 end
--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -5,8 +5,9 @@ RSpec.describe Dish, type: :model do
   context 'Validation' do
     it{ should validate_presence_of(:title) }
     it{ should validate_presence_of(:description) }
-    it{ should validate_numericality_of(:cost).is_greater_than_or_equal_to(0) }
     it{ should validate_presence_of(:fan_id) }
+    it{ should validate_numericality_of(:cost).is_greater_than_or_equal_to(0) }
+    it{ should validate_numericality_of(:pax).only_integer }
   end
 
   context 'Association' do
