d651354475ce5997ab24f19783408386dd815569
wayne <wayne@futureworkz.com>
2015-05-27 10:50:55 +0700

    Update db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,16 +9,22 @@
 Fan.destroy_all
 Dish.destroy_all
 
+fan = Fan.new(name: 'wayne', email: 'wayne@futureworkz.com', password: 12345678)
+fan.skip_confirmation!
+fan.save!
+
 Dish.create!(title: 'Chicken Rice',
              description: 'Receipe: 1 Whole chicken, salt, pepper, galic, pandan leave.',
              cost: 20,
              pax: 3,
              published: true,
-             vegetarian: false)
+             vegetarian: false,
+             fan_id: Fan.first.id)
 
 Dish.create!(title: 'Curry Chicken',
              description: 'Receipe: Chicken wing, curry paste, salt, pepper, galic, pandan leave.',
              cost: 50,
              pax: 5,
              published: true,
-             vegetarian: false)
+             vegetarian: false,
+             fan_id: Fan.first.id)
