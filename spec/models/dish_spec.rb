e0c2188a4c21e3ad3e7836e619b5b31fbf7a5542
wayne <wayne@futureworkz.com>
2015-05-28 13:26:35 +0700

    Refactor Dish model and specs

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 RSpec.describe Dish, type: :model do
   
   context 'Validation' do
-    it{ should validate_presence_of(:title) }
-    it{ should validate_presence_of(:description) }
-    it{ should validate_presence_of(:fan_id) }
-    it{ should validate_numericality_of(:cost).is_greater_than_or_equal_to(0) }
-    it{ should validate_numericality_of(:pax).only_integer }
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:fan_id) }
+    it { should validate_numericality_of(:cost).is_greater_than_or_equal_to(0) }
+    it { should validate_numericality_of(:pax).only_integer }
   end
 
   context 'Association' do
-    it{ should belong_to (:fan) }
+    it { should belong_to (:fan) }
   end
 end
